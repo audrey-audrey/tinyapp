95dacbe897e60796f195c6e425664672a628bb74
audrey-audrey <audreyuu@gmail.com>
2020-11-13 01:35:52 +0000

    Adding documents

--- test/helperTest.js
+++ test/helperTest.js
@@ -4,29 +4,29 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user object with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedOutput = testUsers["userRandomID"];
     
-    assert.equal(user, expectedOutput)
+    assert.equal(user, expectedOutput);
   });
 
   it('should return undefined with invalid email', function() {
-    const user = getUserByEmail("bob@loblaw.com", testUsers)
+    const user = getUserByEmail("bob@loblaw.com", testUsers);
     const expectedOutput = undefined;
     
-    assert.equal(user, expectedOutput)
+    assert.equal(user, expectedOutput);
   });
 });
